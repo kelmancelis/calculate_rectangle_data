117736e7d55e0a82ab8d256760ab2913e29719c8
Kelman Celis <kelmancelis@gmail.com>
2021-04-26 00:01:08 -0300

    initial commit hello word

---
+++ cmake-build-debug/CMakeFiles/calculate_rectangle_data.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calculate_rectangle_data.dir/main.c.o"
+  "calculate_rectangle_data"
+  "calculate_rectangle_data.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/calculate_rectangle_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
